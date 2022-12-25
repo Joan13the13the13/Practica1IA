88b07c076e2ca21a1acaa88a60b89a9824ef3ae7
Joan Balaguer Llagostera <79452171+Joan13the13the13@users.noreply.github.com>
2022-12-25 11:04:12 +0100

    Add files via upload

---
+++ code/__main__.py
@@ -0,0 +1,12 @@
+from practica1 import agent, joc
+
+
+def main():
+    rana = agent.Rana("Miquel")
+    #rana2 = agent.Rana("Diego")
+    lab = joc.Laberint([rana], parets=True)
+    lab.comencar()
+
+
+if __name__ == "__main__":
+    main()
